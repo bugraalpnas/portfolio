dc06f7221faf9e3a94832c3fa30b4a8a0d5b53c9
Bugra Alp Nas <65543414+bugraalpnas@users.noreply.github.com>
2025-08-20 22:58:23 +0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -29,7 +29,7 @@ export default function Footer() {
           <p className="text-xs text-muted-foreground">
             Made with ❤️ by{" "}
             <Link
-              href="https://github.com/bugraalpnas"
+              href="https://github.com/wendoj"
               target="_blank"
               passHref
               className="text-foreground transition hover:text-primary"
